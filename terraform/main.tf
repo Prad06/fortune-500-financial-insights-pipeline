dd3f578fcc5789f041418202164fd58bcc60d008
Pradnyesh Choudhari <51751752+Prad06@users.noreply.github.com>
2024-12-02 00:54:39 -0500

    GCS Sequence Fixed (#3)
    * created the docker container for airflow to connect to GCP and push the data
    
    * fixing gcs upload sequence

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,7 @@ terraform {
 provider "google" {
   project = var.project
   region  = var.region
-  // credentials = file(var.credentials)  # Use this if you do not want to set env-var GOOGLE_APPLICATION_CREDENTIALS
+  credentials = file(var.credentials)  # Use this if you do not want to set env-var GOOGLE_APPLICATION_CREDENTIALS
 }
 
 # Data Lake Bucket
@@ -40,15 +40,81 @@ resource "google_storage_bucket" "data-lake-bucket" {
   force_destroy = true
 }
 
-# DWH
-# Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/bigquery_dataset
-resource "google_bigquery_dataset" "dataset" {
-  dataset_id = var.BQ_DATASET
-  project    = var.project
-  location   = var.region
-}
+# resource "google_storage_bucket" "dataproc-bucket" {
+#   name     = "${local.dataproc_bucket}_${var.project}" # Concatenating DL bucket & Project name for unique naming
+#   location = var.region
+
+#   # Optional, but recommended settings:
+#   storage_class               = var.storage_class
+#   uniform_bucket_level_access = true
+
+#   versioning {
+#     enabled = true
+#   }
+
+#   lifecycle_rule {
+#     action {
+#       type = "Delete"
+#     }
+#     condition {
+#       age = 30 // days
+#     }
+#   }
+
+#   force_destroy = true
+# }
+# # DWH
+# # Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/bigquery_dataset
+
+# resource "google_bigquery_dataset" "dataset" {
+#   dataset_id = var.BQ_DATASET
+#   project    = var.project
+#   location   = var.region
+# }
+
+# # Dataproc cluster
+# # Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/dataproc_cluster
+
+# resource "google_dataproc_cluster" "spark-cluster" {
+#   name                          = var.DATAPROC_CLUSTER
+#   region                        = var.region
+#   graceful_decommission_timeout = "120s"
+#   labels = {
+#     project = var.project
+#   }
 
 
-output "bucket" {
-  value = google_storage_bucket.data-lake-bucket
-}
+#   # You can check these config at the console Create a Dataproc cluster on Compute Engine and check `Equivalent Command Line` for the values
+#   cluster_config {
+#     staging_bucket = resource.google_storage_bucket.dataproc-bucket.name
+
+#     master_config {
+#       num_instances = 1
+#       machine_type  = "e2-standard-4"
+#       disk_config {
+#         boot_disk_type    = "pd-ssd"
+#         boot_disk_size_gb = 200
+#       }
+#     }
+#     preemptible_worker_config {
+#       num_instances = 0
+#     }
+
+#     # Override or set some custom properties
+#     software_config {
+#       image_version = "2.0.35-debian10"
+#       override_properties = {
+#         "dataproc:dataproc.allow.zero.workers" = "true"
+#       }
+#     }
+
+#     gce_cluster_config {
+#       tags = ["project", var.project]
+#       # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+#       service_account = var.service_account
+#       service_account_scopes = [
+#         "cloud-platform"
+#       ]
+#     }
+#   }
+# }
